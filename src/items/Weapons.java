4fc9450b1b4809a13dfbee5dceff12e546eb4e69
mshems <mshems@u.rochester.edu>
2017-03-07 13:55:01 -0500

    release 0.1.0

--- src/items/Weapons.java
+++
@@ -1,8 +0,0 @@
-package items;
-import app.*;
-
-public class Weapons {
-	public static final Weapon ssword = new Weapon("Shortsword", new DiceRoll(1,6));
-	public static final Weapon straxe = new Weapon("Axe of Strength", new DiceRoll(1,10), new int[]{Attribute.STR}, new int[]{2});
-	public static final Weapon lsword = new Weapon("Longsword", new DiceRoll(1,8));
-}
